ab58e7446270b8096b0191add323db85c526697c
Ali Ayad <alimagedayad@users.noreply.github.com>
2023-06-03 18:52:42 +0300

    feat(contact): models

--- contact/models/contact.py
+++ contact/models/contact.py
@@ -1,3 +1,14 @@
+import uuid
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
 # Create your models here.
+class Contact(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.TextField()
+    numbers = ArrayField(
+        models.CharField(
+            max_length=11, 
+            blank=False
+        ), blank=False, default=list
+    )
